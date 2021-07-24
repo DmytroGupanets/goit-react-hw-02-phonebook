87be235a8a49792f2f022151515b177eda0e3834
Gupanets Dmytro <dmgpnts@gmail.com>
2021-07-24 17:16:34 +0300

    complete hw

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
+import ContactForm from "./contactForm/ContactForm";
+import ContactList from "./contactsList/ContactsList";
+import Filter from "./filter/Filter";
 
 class App extends Component {
-  state = {};
+  state = {
+    contacts: [],
+    filter: "",
+  };
+
+  onHandleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  addNewContact = (contact) => {
+    this.setState((prevState) => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
+
+  onClickDeleteContact = (id) => {
+    this.setState((prevState) => ({
+      contacts: prevState.contacts.filter((contact) => contact.id !== id),
+    }));
+  };
 
   render() {
-    return <></>;
+    return (
+      <>
+        <h2>Phonebook</h2>
+        <ContactForm
+          addNewContact={this.addNewContact}
+          contacts={this.state.contacts}
+        />
+        <h2>Contacts</h2>
+        <Filter onHandleChange={this.onHandleChange} />
+        <ContactList
+          onClickDeleteContact={this.onClickDeleteContact}
+          contacts={this.state.contacts}
+          filterValue={this.state.filter}
+        />
+      </>
+    );
   }
 }
 
